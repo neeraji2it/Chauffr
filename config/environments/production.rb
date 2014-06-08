9ccac5947c303167a0bb838819119e4f39536e17
rails <rails@rails-MS-7267.(none)>
2014-06-08 20:02:00 +0530

    small changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'http://chauffr-dev.herokuapp.com' }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'oe.member1@gmail.com',
+    :password => "0nlineeduc@ti0n",
+    
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 end
