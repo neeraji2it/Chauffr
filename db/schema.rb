cbe0288053038a39cbc2c91a6e22f8af7e455486
rails <rails@rails-MS-7267.(none)>
2014-06-09 14:45:03 +0530

    some changes on users and profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609052445) do
+ActiveRecord::Schema.define(version: 20140609083120) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140609052445) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "unicode"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
