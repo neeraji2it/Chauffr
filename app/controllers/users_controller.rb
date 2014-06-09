276c9d92130ffd718574a1554ea79b6ac8a6ea2a
rails <rails@rails-MS-7267.(none)>
2014-06-09 23:23:15 +0530

    deleted unwanted files

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < Devise::RegistrationsController  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to contact_profiles_path
-    end
-  end
-end
